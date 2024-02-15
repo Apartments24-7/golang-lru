848863975caed1682899cd60686b5a95d1fe0f1c
Brian Arita <barita@apartments247.com>
2024-02-15 19:01:39 +0000

    updated to use packages

--- expirable/expirable_lru.go
+++ expirable/expirable_lru.go
@@ -7,7 +7,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/hashicorp/golang-lru/v2/internal"
+	"github.com/hashicorp/golang-lru/internal"
 )
 
 // EvictCallback is used to get a callback when a cache entry is evicted
